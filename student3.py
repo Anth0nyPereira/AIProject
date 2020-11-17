edf3210f101778356e1c723b69241722ff372558
unknown <lvalentim@ua.pt>
2020-11-17 14:32:23 +0000

    Retirei o dominio, acabei a solução inicial em arvore e comentei tudo

--- student3.py
+++ student3.py
@@ -1,46 +1,23 @@
 from mapa import Map
 
-
-class SearchDomain(ABC):
-
-    # construtor
-    @abstractmethod
-    def __init__(self):
-        pass
-
-    # lista de accoes possiveis num estado
-    @abstractmethod
-    def actions(self, state):
-        pass
-
-    # resultado de uma accao num estado, ou seja, o estado seguinte
-    @abstractmethod
-    def result(self, state, action):
-        pass
-
-    # custo de uma accao num estado
-    @abstractmethod
-    def cost(self, state, action):
-        pass
-
-    # custo estimado de chegar de um estado a outro
-    @abstractmethod
-    def heuristic(self, state, goal):
-        pass
-
-    # "goal" is satisfied in "state"
-    @abstractmethod
-    def satisfies(self, state, goal):
-        pass
-
+#Há muita coisa comentada que são partes das implementações antigas que não me pareceram apropriadas / uteis mas deixei caso venham a ser necessárias
+#Retirei o dominio porque aquilo que o dominio fazia é o que estamos a fazer com o mapa, logo acho que o mapa é o nosso dominio.
+#A forma como isto funciona é, criamos uma arvore, que tem um problema, o problema é o estado inicial do mapa simplesmente.
+#O problema vai dar origem ao root node que vai automaticamente gerar todos os outros nodes da arvore sobre 2 condições:
+#A jogada é possivel
+#A jogada não leva caixas para cantos
+#Depois o search da arvore vai percorrer todos os nodes até encontrar um que passe no problem.goal_test, ou seja que tenha todos os diamantes preenchidos por caixas
+#Quando encontrar vai dar-nos as jogadas que temos que fazer em forma de um array através do get_play
 
 class SearchProblem:
-    def __init__(self, domain, initial, goal):
-        self.domain = domain
+    def __init__(self, initial):   #Initial é o estado do mapa quando o nivel começa
         self.initial = initial
-        self.goal = goal
     def goal_test(self, state):
-        return self.domain.satisfies(state,self.goal)
+        if(state.empty_goals() == 0):  #Se os goals tem todos caixas em cima então está ganho
+            return True
+
+        else:
+            return False
 
 class Node:
     def __init__(self, tree, parent=None, key=None, state_map):
@@ -49,7 +26,8 @@ class Node:
         self.key = key
         self.state_map = state_map
         self.children = []
-        create_childs()
+        create_childs()                 #Cria os filhos
+        tree.open_nodes.append(self)    #E junta-se ao grupo de nós a explorar
 
     def create_childs(self):
 
@@ -66,16 +44,22 @@ class Tree:
     def __init__(self, problem):
         self.problem = problem
         self.root = Node(self, problem.initial) #problem initial é o estado inicial do nivel
-        self.open_nodes = [self.root]
-        self.solution = None
+        self.open_nodes = [self.root]           #nodes a explorar
+        self.solution = None                    #node da jogada final, não me parece importante mas pode vir a ser
+        self.visitedStates = []                 #estados em que já tivemos, para ele não andar de um lado para o outro sem fazer nada
 
         def search(self):
             while self.open_nodes != []:
                 node = self.open_nodes.pop(0)
-                if self.problem.goal_test(node.state_map):
-                    self.terminal = len(self.open_nodes) + 1
+                if(node.state_map in self.visitedStates):                   #Evitar que ele volte a estados em que já esteve (não sei se isto não vai causar problemas mais tarde)
+                    continue                                                #Se ele não conseguir encontrar soluções retirem esta linha, pode estar a causar problemas
+                if self.problem.goal_test(node.state_map):                  #Verificar se é solução
+                    #self.terminal = len(self.open_nodes) + 1               #Não me parece necessário mas posso estar errado
                     self.solution = node
                     return self.get_play(node)
+                else:
+                    self.visitedStates.append(node.state_map)
+                                                                            # Tudo isto era geração de nós que já estamos a fazer com o create_childs no Node
                 #self.non_terminal += 1
                 #node.children = []
                 #self.nodeList.append(node)
@@ -91,7 +75,7 @@ class Tree:
                 #self.add_to_open(node.children)
             #return None
 
-        def get_play(self, node):
+        def get_play(self, node):                        #Basicamente o mesmo que o get_path era nas cidades mas já nos dá o array com as teclas a primir para resolver o nivel
             if node.parent == None:
                 return []
             play = self.get_play(node.parent)
